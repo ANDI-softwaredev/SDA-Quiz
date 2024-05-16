946fad5921f92eeef70c35c80721384ae66f8286
Andile <112043128+ANDI-softwaredev@users.noreply.github.com>
2024-05-16 05:55:12 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the name of Hosea's daughter in Chapter 1?",
+    answer: "Not pitied",
+    options: [
+      "Jezreel",
+      "Dinah",
+      "She has obtained pity",
+      "Not pitied"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Give chapter and verse: God did not give us a spirit of fear but a spirit of power and love and self-control",
+    answer: "1:7",
+    options: [
+      "2:7",
+      "1:7",
+      "1:3",
+      "2:3"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is Timothy's mother?",
+    answer: "Lois",
+    options: [
+      "Enuice",
+      "Lois",
+      "Martha",
+      "Rebecca"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Fill in: from Asher, Pa'giel the son of _____",
+    answer: "Ochran",
+    options: [
+      "Gideo'ni",
+      "Ammishad'dai",
+      "Deu'el",
+      "Ochran"
+    ]
+  },
+{
+    numb: 5,
+    question: "How many cahpters in the book of John?",
+    answer: "21",
+    options: [
+      "21",
+      "30",
+      "20",
+      "31"
+    ]
+  },
+];
